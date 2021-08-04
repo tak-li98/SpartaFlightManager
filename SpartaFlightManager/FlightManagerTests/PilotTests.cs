656a4a4b6c8d965275d5c49808ac34a61cb2bf09
Tak Li <takwing.li98@gmail.com>
2021-08-04 22:22:26 +0100

    added flight details manager

---
+++ SpartaFlightManager/FlightManagerTests/PilotTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Database;
+using Manager;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FlightManagerTests
+{
+    public class PilotTests
+    {
+        private PilotManager _pilotManager = new PilotManager();
+
+        [SetUp]
+        public void Setup()
+        {
+            var newPilot = new Pilot() { Title = "Mr", FirstName = "Dummy", LastName = "Data" };
+            using(var db = new SpartaFlightContext())
+            {
+                db.Pilots.Add(newPilot);
+                db.SaveChanges();
+            }
+        }
+        [Test]
+        public void CheckIfNewPilotAddedDatabaseIncreasesBy1()
+        {
+            using (var db = new SpartaFlightContext())
+            {
+                var initialCount = db.Pilots.Count();
+                _pilotManager.Create( "Dummy","Data", "Mr");
+                var finalCount = db.Pilots.Count();
+                Assert.That(finalCount - initialCount, Is.EqualTo(1));
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            using(var db = new SpartaFlightContext())
+            {
+                var removePilots = db.Pilots.Where(p => p.FirstName == "Dummy" && p.LastName == "Data");
+                foreach (var removePilot in removePilots)
+                {
+                    db.Pilots.RemoveRange(removePilot);
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- SpartaFlightManager/Manager/FlightDetailsManager.cs
+++ SpartaFlightManager/Manager/FlightDetailsManager.cs
@@ -3,10 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Database;
+using System.Diagnostics;
 
 namespace Manager
 {
-    class FlightDetailsManager
+    public class FlightDetailsManager
     {
+        public FlightDetail SelectedFlightDetail { get; set; }
+
+        public void SetFlightDetail(object selectedItem)
+        {
+            SelectedFlightDetail = (FlightDetail)selectedItem;
+        }
+
+        public List<FlightDetail> RetrieveAll()
+        {
+            using (var db = new SpartaFlightContext())
+            {
+                return db.FlightDetails.ToList();
+            }
+        }
+        public void Create(int flightId, int pilotId, int airlineId, int planeId, int passengerNumber, int flightDuration, bool archive=false)
+        {
+            var newFDetails = new FlightDetail() {FlightId = flightId,PilotId = pilotId, AirlineId=airlineId
+                ,PlaneId=planeId,PassengerNumber=passengerNumber,FlightDuration=flightDuration, Archive = archive };
+            using (var db = new SpartaFlightContext())
+            {
+                db.FlightDetails.Add(newFDetails);
+                db.SaveChanges();
+            }
+        }
+
+        public bool Update(int flightDetailsId,int flightId, int pilotId, 
+            int airlineId, int planeId, int passengerNumber, int flightDuration, bool archive = false)
+        {
+            using (var db = new SpartaFlightContext())
+            {
+                var updateFDetails = db.FlightDetails.Where(f => f.FlightDetailsId == flightDetailsId).FirstOrDefault();
+                if (updateFDetails == null)
+                {
+                    return false;
+                }
+                updateFDetails.FlightId = flightId;
+                updateFDetails.PilotId = pilotId;
+                updateFDetails.AirlineId = airlineId;
+                updateFDetails.PlaneId = planeId;
+                updateFDetails.PassengerNumber = passengerNumber;
+                updateFDetails.FlightDuration = flightDuration;
+                updateFDetails.Archive = archive;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"Error updating flight detail {flightDetailsId}");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool Delete(int flightDetailsId)
+        {
+            using (var db = new SpartaFlightContext())
+            {
+                var delFDetails = db.FlightDetails.Where(f => f.FlightDetailsId == flightDetailsId);
+                if (delFDetails == null)
+                {
+                    return false;
+                }
+                foreach (var delFDetail in delFDetails)
+                {
+                    db.FlightDetails.RemoveRange(delFDetail);
+                }
+                db.SaveChanges();
+            }
+            return true;
+        }
     }
 }
+
