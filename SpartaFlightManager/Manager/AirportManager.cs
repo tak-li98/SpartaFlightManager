83f7d8b10eeaab5487067b824b11ee830b99dbb8
Tak Li <takwing.li98@gmail.com>
2021-08-05 22:05:57 +0100

    Added airport manager to return airport objects

---
+++ SpartaFlightManager/Manager/AirportManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace Manager
+{
+    public class AirportManager
+    {
+        public List<Airport> ReturnAirports()
+        {
+            using (var db = new SpartaFlightContext())
+            {
+                return db.Airports.ToList();
+            }
+        }
+    }
+}
