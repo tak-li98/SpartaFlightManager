73b5cc59e58f06883f7391661bd7ee3f6f6b7b25
Tak Li <takwing.li98@gmail.com>
2021-08-07 17:45:05 +0100

    Added other button functionality to pilot window

--- SpartaFlightManager/GUI/PilotWindow.xaml.cs
+++ SpartaFlightManager/GUI/PilotWindow.xaml.cs
@@ -25,16 +25,16 @@ namespace GUI
         {
             double screenWidth = SystemParameters.PrimaryScreenWidth;
             double screenHeight = SystemParameters.PrimaryScreenHeight;
-            double windowWidth = this.Width;
-            double windowHeight = this.Height;
-            this.Left = (screenWidth / 2) - (windowWidth / 2);
-            this.Top = (screenHeight / 2) - (windowHeight / 2);
+            double windowWidth = Width;
+            double windowHeight = Height;
+            Left = (screenWidth / 2) - (windowWidth / 2);
+            Top = (screenHeight / 2) - (windowHeight / 2);
         }
         public void OpenWindow(Window window)
         {
             window.Show();
             window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            this.Close();
+            Close();
         }
         public PilotWindow()
         {
@@ -48,42 +48,43 @@ namespace GUI
         }
         private void FlightBoardButton_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenWindow(new MainWindow());
         }
 
         private void AirportsButton_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenWindow(new AirportWindow());
         }
 
         private void AirlinesButton_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenWindow(new AirlineWindow());
         }
 
         private void PlanesButton_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenWindow(new PlaneWindow());
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            EditPilotButton.IsEnabled = true;
+            _pilotManager.SetSelectedPilot(pilotBoard.SelectedItem);
         }
 
         private void AddPilotButton_Click(object sender, RoutedEventArgs e)
         {
-
+            pilotBoard.Visibility = Visibility.Hidden;
         }
 
         private void minimiseBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            WindowState = WindowState.Minimized;
         }
 
         private void exitBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            Application.Current.Shutdown();
         }
 
         private void EditPilotButton_Click(object sender, RoutedEventArgs e)
