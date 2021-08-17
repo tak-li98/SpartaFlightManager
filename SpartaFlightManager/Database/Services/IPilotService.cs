6d23d76ce351c51b2cad798a0996aa108ca850d4
Tak Li <takwing.li98@gmail.com>
2021-08-17 11:22:16 +0100

    Added IPilotService class

---
+++ SpartaFlightManager/Database/Services/IPilotService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database.Services
+{
+    public interface IPilotService
+    {
+        public List<Pilot> GetPilotList();
+        public Pilot GetPilotById(string pilotId);
+        public void CreatePilot(Pilot p);
+        public void SavePilotChanges();
+        public void RemovePilot(Pilot p);
+    }
+}
